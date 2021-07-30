224875187d70d06bce51062af75b083167938245
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-07-31 00:47:44 +0600

    Mathematical Operation in Js

---
+++ math-operation.js
@@ -0,0 +1,21 @@
+// Declare Variable
+var numberOne = 10;
+var numberTwo = 2;
+
+// Mathmetical Operation
+var sumation = numberOne + numberTwo;
+var subtraction = numberOne - numberTwo;
+var multiplication = numberOne * numberTwo;
+var division = numberOne / numberTwo;
+
+// Print Result
+console.log(sumation);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+
+// Operation With Out New Variable
+numberOne = numberOne + 50;
+numberTwo += 100;
+console.log(numberOne);
+console.log(numberTwo);
