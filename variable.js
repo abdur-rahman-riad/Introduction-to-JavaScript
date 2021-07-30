b781acb0e2511695e7d0906171af8c5ec9686ef1
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-07-30 23:59:23 +0600

    Types of Variable in Js

---
+++ variable.js
@@ -0,0 +1,12 @@
+// Numeric Variable
+var age = 21;
+// String Variable
+var myname = "Riad Abdur Rahman";
+var university = "Daffodil International University";
+// Boolean Variable
+var semesterpass = true;
+var semesterfail = false;
+// Print in Console
+console.log(myname);
+console.log(age);
+console.log(university);
